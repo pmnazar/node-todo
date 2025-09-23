342d48b2e642c25eea55c500cdcec57e6f89900d
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-23 17:09:07 +0300

    chore(frontend): replace Vanilla JS with Angular setup

--- frontend/todo/src/app/features/auth/register/register.component.ts
+++ frontend/todo/src/app/features/auth/register/register.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { passwordMatchValidator } from '../../../shared/validators';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  form = new FormGroup<{
+    username: FormControl<string | null>;
+    email: FormControl<string | null>;
+    passwordGroup: FormGroup<{
+      password: FormControl<string | null>;
+      confirmPassword: FormControl<string | null>;
+    }>;
+  }>({
+    username: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    passwordGroup: new FormGroup<{
+      password: FormControl<string | null>;
+      confirmPassword: FormControl<string | null>;
+    }>(
+      {
+        password: new FormControl<string | null>(null),
+        confirmPassword: new FormControl<string | null>(null),
+      },
+      { validators: passwordMatchValidator() },
+    ),
+  });
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      const username = this.form.value.username!;
+      const email = this.form.value.email!;
+      const password = this.form.value.passwordGroup?.password!;
+
+      this.authService.register(username, email, password).subscribe({
+        next: (res) => {
+          if (res) {
+            this.router.navigate(['/login']);
+          }
+        },
+      });
+    }
+  }
 }
