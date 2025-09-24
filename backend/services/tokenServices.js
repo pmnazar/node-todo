1d0b97ca66d22266b5b17cf7e63a3014f2da1eea
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-24 17:20:03 +0300

    chore(auth): update accessToken lifetime to 15 minutes

--- backend/services/tokenServices.js
+++ backend/services/tokenServices.js
@@ -5,7 +5,7 @@ export function generateAccessToken(user) {
   return jwt.sign(
     { id: user.id, username: user.username },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "10s" },
+    { expiresIn: "15m" },
   );
 }
 
