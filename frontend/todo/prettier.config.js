3e0f38ab7003d8ac8af5604dffa1fa5aebc0539d
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-24 20:43:56 +0300

    chore: add Prettier config for HTML, TS, JS, and JSON

--- frontend/todo/prettier.config.js
+++ frontend/todo/prettier.config.js
@@ -1,10 +1,31 @@
 /** @type {import("prettier").Config} */
 const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 80,
+  endOfLine: "lf",
   overrides: [
     {
       files: "*.html",
       options: {
         parser: "angular",
+      },
+    },
+    {
+      files: ["*.ts", "*.js"],
+      options: {
+        parser: "typescript",
+      },
+    },
+    {
+      files: ["*.json"],
+      options: {
+        parser: "json",
         tabWidth: 2,
       },
     },
