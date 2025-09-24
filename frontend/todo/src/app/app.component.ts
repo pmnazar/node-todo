c7aa180edf99bc2dd5780f52664fe5afa21c6c46
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-24 10:54:40 +0300

    hotfix: fix broken imports after component relocation

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
