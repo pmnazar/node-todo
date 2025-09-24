32afe0106f608b8109ec25e36c77c7d505b109ca
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-24 20:51:14 +0300

    chore(auth): adjust access token expiresIn for testing on prod

--- backend/services/tokenServices.js
+++ backend/services/tokenServices.js
@@ -5,7 +5,7 @@ export function generateAccessToken(user) {
   return jwt.sign(
     { id: user.id, username: user.username },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "15m" },
+    { expiresIn: "10s" },
   );
 }
 
