3ef64c118871b03fee6ff848d83024cf2c0896c3
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-25 12:49:42 +0300

    test(jest): setup Jest and add first test for TopBarComponent

--- frontend/todo/src/app/core/services/user.service.ts
+++ frontend/todo/src/app/core/services/user.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
 import { User } from '../../shared/models/user.model';
 
 @Injectable({
--- frontend/todo/src/app/shared/models/user.model.ts
+++ frontend/todo/src/app/shared/models/user.model.ts
@@ -1,5 +1,5 @@
 export interface User {
   _id?: string;
   username: string;
-  emali: string;
+  email: string;
 }
